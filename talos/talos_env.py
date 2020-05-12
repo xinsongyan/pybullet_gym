c1908442240437e5ea4f4dc8aaee5028ecf84a2c
xinsongyan <xinsongyan@gmail.com>
2020-05-12 21:37:01 +0100

    add talos_lower_body_mesh_updated.urdf

--- talos/talos_env.py
+++ talos/talos_env.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     sim_rate = 1000
     g = 9.81
-    urdfPath = os.getcwd() + '/urdf/talos_lower_body.urdf'
+    urdfPath = os.getcwd() + '/urdf/talos_lower_body_mesh_updated.urdf'
 
     homing_config = np.zeros(12)
 
